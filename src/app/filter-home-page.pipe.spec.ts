4a3864304cbff1ab881be737d07db0666bf06f45
Mark LaRosa <mark.larosa2@gmail.com>
2018-03-03 18:58:10 -0500

    add pipe for filtering home page

---
+++ src/app/filter-home-page.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterHomePagePipe } from './filter-home-page.pipe';
+
+describe('FilterHomePagePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterHomePagePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/filter-home-page.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filterHomePage'
+})
+export class FilterHomePagePipe implements PipeTransform {
+
+  transform(pages: string[]): any {
+    if (pages) {
+      return pages.filter(page => page !== 'HOMEPAGE');
+    }
+
+    return [];
+  }
+
+}
